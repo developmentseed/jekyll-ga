86ced51034fa28f35bd0e1e3174b06254e07f373
Ikillnukes <ikillnukes1@gmail.com>
2019-03-21 18:50:47 +0100

    Renamed '@@response_data' to '@response_data'

--- lib/jekyll-ga-v2/jekyll-ga-v2.rb
+++ lib/jekyll-ga-v2/jekyll-ga-v2.rb
@@ -12,7 +12,7 @@ module Jekyll
   class GoogleAnalytics < Generator
     priority :highest
       
-    @@response_data = nil
+    @response_data = nil
     @past_response = nil
     @headers = nil
 
@@ -42,7 +42,7 @@ module Jekyll
 
       # Now lets check for the cache file and how old it is
       if File.exist?(cache_file_path) and ((Time.now - File.mtime(cache_file_path)) / 60 < refresh_rate) and !ga["debug"]
-        @@response_data = JSON.parse(File.read(cache_file_path));
+        @response_data = JSON.parse(File.read(cache_file_path));
       else
         analytics = Google::Apis::AnalyticsV3::AnalyticsService.new
           
@@ -86,30 +86,30 @@ module Jekyll
             raise RuntimeError, "Check errors from Google Analytics"
         end
 
-        @@response_data = response
+        @response_data = response
 
         # Write the response data
         File.open(cache_file_path, "w") do |f|
-          f.write(@@response_data.to_json)
+          f.write(@response_data.to_json)
         end
 
         # Debug statments (TODO: implement a tag for this)
         # Implement a macro to use in the ga[filters] called :currentUrl (ga:pagePath=@/my/url) (from: https://stackoverflow.com/questions/46039271/google-analytics-api-get-page-views-by-url)
         # https://stackoverflow.com/questions/27936532/400-invalid-value-gapagepath-for-filters-parameter
-        if @@response_data.kind_of?(Google::Apis::AnalyticsV3::GaData) and !@@response_data.rows.nil?
-            results = @@response_data.rows
+        if @response_data.kind_of?(Google::Apis::AnalyticsV3::GaData) and !@response_data.rows.nil?
+            results = @response_data.rows
 
             # if ga["debug"]
-            #    Jekyll.logger.info "Jekyll GoogleAnalytics:", @@response_data.to_json
+            #    Jekyll.logger.info "Jekyll GoogleAnalytics:", @response_data.to_json
             # end
         end
           
       end
         
-      Jekyll.logger.info "Jekyll GoogleAnalytics (pre-prod):",@@response_data.to_json
+      Jekyll.logger.info "Jekyll GoogleAnalytics (pre-prod):",@response_data.to_json
         
       # Get keys from columnHeaders
-      @headers = @@response_data.column_headers.collect { |header| header.name.sub("ga:", "") }
+      @headers = @response_data.column_headers.collect { |header| header.name.sub("ga:", "") }
         
       # Loop through pages && posts to add the stats object value
         
@@ -153,10 +153,10 @@ module Jekyll
         
        # Transpose array into hash using columnHeaders    
        if page_type == "page"
-          data = @@response_data.rows.select { |row| row[0] == filter_url(inst.dir + inst.name) }.collect { |row| Hash[ [@headers, row].transpose ] }[0]
+          data = @response_data.rows.select { |row| row[0] == filter_url(inst.dir + inst.name) }.collect { |row| Hash[ [@headers, row].transpose ] }[0]
           past_data = @past_response.nil? or !@past_response.nil? and @past_response.rows.nil? ? nil : @past_response.rows.select { |row| row[0] == filter_url(inst.dir + inst.name) }.collect { |row| Hash[ [@headers, row].transpose ] }[0]
        elsif page_type == "post"
-          data = @@response_data.rows.select { |row| row[0] == filter_url(inst.url.to_s) }.collect { |row| Hash[ [@headers, row].transpose ] }[0]
+          data = @response_data.rows.select { |row| row[0] == filter_url(inst.url.to_s) }.collect { |row| Hash[ [@headers, row].transpose ] }[0]
           past_data = @past_response.nil? or !@past_response.nil? and @past_response.rows.nil? ? nil : @past_response.rows.select { |row| row[0] == filter_url(inst.url.to_s) }.collect { |row| Hash[ [@headers, row].transpose ] }[0]
        elsif page_type == "site"
           data = get_site_data(false)
@@ -167,36 +167,39 @@ module Jekyll
            return nil
        end
         
-       pre_data = {}
-        
-       data.each { |key, value|
-            present_value = value.to_f
+       # Create diff_xxx and xxx_perc keys for data    
+       if ga["compare_period"]
+           pre_data = {}
            
-            past_value = nil
-           
-            if past_data.kind_of?(Hash)
-               past_value = past_data.fetch(key, 0.0).to_f
-            else
-               past_value = 0.0 
-            end
-           
-            if float?(value) and float?(past_value) # Filter for pagePath (not float or integer value)
-                diff_value = present_value - past_value
-                perc_value = present_value / past_value * 100.0
-                
-                pre_data.store("diff_#{key}", diff_value)
-                pre_data.store("#{key}_perc", perc_value == Float::INFINITY ? "∞" : perc_value.to_s)
-            end
-       }
-        
-       data.merge!(pre_data)
+           data.each { |key, value|
+                present_value = value.to_f
+
+                past_value = nil
+
+                if past_data.kind_of?(Hash)
+                   past_value = past_data.fetch(key, 0.0).to_f
+                else
+                   past_value = 0.0 
+                end
+
+                if float?(value) and float?(past_value) # Filter for pagePath (not float or integer value)
+                    # Thanks to: https://stackoverflow.com/q/31981133/3286975
+                    diff_value = present_value - past_value
+                    perc_value = present_value / past_value * 100.0
+
+                    pre_data.store("diff_#{key}", diff_value)
+                    pre_data.store("#{key}_perc", perc_value == Float::INFINITY ? "∞" : perc_value.to_s)
+                end
+           }
+
+           data.merge!(pre_data)
+       end
         
        return data
     end
       
     def get_site_data(is_past)
-        data = (is_past ? @past_response : @@response_data).totals_for_all_results
-        
+        data = (is_past ? @past_response : @response_data).totals_for_all_results
         data.keys.each { |k| data[k.sub("ga:", "")] = data[k]; data.delete(k) }
         
         return data;
@@ -227,10 +230,6 @@ module Jekyll
         end
         
         return url
-    end
-      
-    def get_data
-       @@response_data 
     end
       
     def float?(string)
